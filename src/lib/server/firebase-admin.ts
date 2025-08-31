c0f4b82851c84c260ff214e5cd2dce0a47f3862b
cherabear <sammysavage36@gmail.com>
2025-08-31 14:29:55 -0700

    Update firebase-admin.ts

--- src/lib/server/firebase-admin.ts
+++ src/lib/server/firebase-admin.ts
@@ -6,9 +6,9 @@ import { PROJECT_ID, CLIENT_EMAIL, PRIVATE_KEY } from '$env/static/private';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: PROJECT_ID!,
-      clientEmail: CLIENT_EMAIL!,
-      privateKey: (PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+      projectId: process.env.PROJECT_ID,
+      clientEmail: process.env.CLIENT_EMAIL,
+      privateKey: (process.env.PRIVATE_KEY || '').replace(/\\n/g, '\n'),
 
     }),
   });
